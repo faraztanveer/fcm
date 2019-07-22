72c2e536af9b59dc9a28f1627fb9792ec61a6a01
faraztanveer <tanveer.faraz07@gmail.com>
2019-07-22 11:58:25 +0500

    changes reverted

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,6 +1,6 @@
 // Give the service worker access to Firebase Messaging.
-importScripts('https://www.gstatic.com/firebasejs/6.3.1/firebase-app.js')
-importScripts('https://www.gstatic.com/firebasejs/6.3.1/firebase-messaging.js')
+importScripts('https://www.gstatic.com/firebasejs/4.9.1/firebase-app.js')
+importScripts('https://www.gstatic.com/firebasejs/4.9.1/firebase-messaging.js')
 
 // Initialize the Firebase app in the service worker by passing in the messagingSenderId.
 var config = {
